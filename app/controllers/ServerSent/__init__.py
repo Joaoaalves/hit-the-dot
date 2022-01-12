ad60325fdf2016f3f9885ce4402137239d15ed0e
root <root@htd.dbsweb.com.br>
2022-01-12 13:06:48 -0300

    .

--- app/controllers/ServerSent/__init__.py
+++ app/controllers/ServerSent/__init__.py
@@ -1,8 +1,4 @@
 from flask import Blueprint, url_for, request, session, stream_with_context, Response
 import json
 from configparser import ConfigParser
-<<<<<<< HEAD
-from app import db, redis
-=======
 from app import db
->>>>>>> 0dd6efaf2d73e75da40675b932021a61df16f97e
