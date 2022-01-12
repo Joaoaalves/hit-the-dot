e284c772b884489d40aafa60b2e68f1e61b4cee2
Joaoaalves <joaoaa21@hotmail.com>
2022-01-12 12:52:15 -0300

    Adicionado uma conexão do redis somente ao chamar a função de detecção de usuarios online

--- app/controllers/ServerSent/__init__.py
+++ app/controllers/ServerSent/__init__.py
@@ -1,5 +1,4 @@
-from flask_sse import sse
 from flask import Blueprint, url_for, request, session, stream_with_context, Response
 import json
 from configparser import ConfigParser
-from app import db, redis
+from app import db
