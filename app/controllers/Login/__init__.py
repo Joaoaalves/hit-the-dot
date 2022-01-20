40ab943eed5f31aa56f931ce03baabeb9a77b9c7
João Alves <joaoaa21@hotmail.com>
2022-01-20 12:43:59 -0300

    Adicionado ipban com persistencia mesmo após reinicialização do servidor

--- app/controllers/Login/__init__.py
+++ app/controllers/Login/__init__.py
@@ -1,4 +1,4 @@
-from app import app, invalid_sessions, users, invalid_sessions, db, recaptcha
+from app import app, invalid_sessions, users, invalid_sessions, db, recaptcha, ip_ban
 from app.controllers.decorators import block_cross_site_requests, login_required, admin_required, is_admin, get_user_object
 from flask import Blueprint, request, url_for, render_template, redirect, session
 import flask
