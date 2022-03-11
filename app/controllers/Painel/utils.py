20a1d0b6481427d1dfb08fa2774f125c3606c62f
João Alves <joaoaa21@hotmail.com>
2022-03-11 13:17:42 -0300

    Adicionado grafico de demandas verificadas no painel principal do funcionario

--- app/controllers/Painel/utils.py
+++ app/controllers/Painel/utils.py
@@ -84,6 +84,7 @@ def render_painel_func(funcionario, start_date, end_date):
     start_date_html = start_date.strftime('%d-%m-%Y')
     end_date_html = end_date.strftime('%d-%m-%Y')
 
+    demandas, demanda_dict = get_demandas(start_date, end_date, funcionario.id)
     
     return render_template('painel-func.html', user=funcionario, painel_active='active',
                                             percentage=percentage,
@@ -97,7 +98,9 @@ def render_painel_func(funcionario, start_date, end_date):
                                             end_date=end_date_html,
                                             media_horas=media_horas,
                                             dias_trabalhados=dias_trabalhados,
-                                            horas_devendo=horas_devendo)
+                                            horas_devendo=horas_devendo,
+                                            demandas=demandas,
+                                            demanda_dict=demanda_dict)
 
 
 def get_percentage_work(segundos_trabalhados, segundos_totais):
